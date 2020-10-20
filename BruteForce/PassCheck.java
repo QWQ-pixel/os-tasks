bccf57c36eb425386c01d4ca74f34afdc24d761c
Valentina <61939190+QWQ-pixel@users.noreply.github.com>
2020-10-20 12:57:58 +0300

    Update PassCheck.java

--- BruteForce/PassCheck.java
+++ BruteForce/PassCheck.java
@@ -15,28 +15,29 @@ public class PassCheck extends Thread{
     @Override
     public void run(){
         try {
-            checkPass(password);
+            checkPass();
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
     }
-    public static void checkPass(ArrayList<String> password) throws NoSuchAlgorithmException {
+    public static void checkPass() throws NoSuchAlgorithmException {
         ArrayList<String> hash=new ArrayList<>();
         hash.add("1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad");
         hash.add("3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b");
         hash.add("74e1bb62f8dabb8125a58852b63bdf6eaef667cb56ac7f7cdba6d7305c50a22f");
-        while (count != 3) {
-             for(int i=0;i<password.size();i++) {
-             CheckPass(password.get(i), hash);
-            if (count == 3) {
-                Instant finish = Instant.now();
-                long elapsed = Duration.between(start, finish).toMillis();
-                System.out.println("Time passed: " + (elapsed/1000)+" sec");
-                break;
-            }
-             }
+       
+        for(int i=0;i<password.size();i++) {
+           CheckPass(password.get(i), hash);
+              if (count == 3) {
+                    Instant finish = Instant.now();
+                    System.out.println("All password are matched");
+                    long elapsed = Duration.between(start, finish).toMillis();
+                    System.out.println("Time passed: " + (elapsed/1000)+" sec");
+                    break;
+                }
             }
         }
+ 
     public static void CheckPass(String password, ArrayList<String> hash) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         byte[] hashInBytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
